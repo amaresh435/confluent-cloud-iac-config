73e31a90aa7d64e7f0c0a74404d31a4db7440b35
AmareshGuligoudar <amareshguligoudar@gmail.com>
2026-07-29 01:15:40 +0530

    Create provider.tf

---
+++ environments/pr/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">=1.9.7"
+
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "~> 2.78"
+    }
+  }
+
+  google = {
+    source = "hashicorp/google"
+    version = ">=6.0"
+  }
+}
+
+provider "confluent" {
+  # Credentials come from CONFLUENT_CLOUD_API_KEY and CONFLUENT_CLOUD_API_SECRET.
+}

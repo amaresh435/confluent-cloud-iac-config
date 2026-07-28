02179fa3cd65bd2cc40b7fb24189af7d17ebd1fb
AmareshGuligoudar <amareshguligoudar@gmail.com>
2026-07-29 00:41:59 +0530

    Create data.tf

---
+++ environments/pr/data.tf
@@ -0,0 +1,11 @@
+data "google_secret_manager_secret_version" "tfm_confluent_api_key" {
+  project = local.gcp_project_id
+  secret  = "terraform-confluent-cloud-api-key"
+  version = "latest"
+}
+
+data "google_secret_manager_secret_version" "tfm_confluent_api_key" {
+  project = local.gcp_project_id
+  secret  = "terraform-confluent-cloud-api-secret"
+  version = "latest"
+}

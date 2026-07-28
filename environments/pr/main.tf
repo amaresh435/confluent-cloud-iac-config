b3ec6e476d4974d976988455f7a2cc02e642b649
AmareshGuligoudar <amareshguligoudar@gmail.com>
2026-07-29 00:45:55 +0530

    Update Terraform configuration for PR environment

--- environments/pr/main.tf
+++ environments/pr/main.tf
@@ -1,21 +1,26 @@
 locals {
-  env_name                = "pr"
-  prod_name               = "fish"
-  resource_prefix         = join("-", [local.prod_name, local.env_name])
-  gcp_project_id           = "vidya-00001"
-  env_location            = "us-central1"
+  env_name                    = "pr"
+  prod_name                   = "fisher"
+  resource_prefix             = join("-", [local.prod_name, local.env_name])
+  gcp_project_id              = "vidya-00001"
+  env_location                = "us-central1"
+  tfm_sa_confluent_api_key    = data.google_secret_manager_secret_version.tfm_confluent_api_key.secret_data
+  tfm_sa_confluent_api_secret = data.google_secret_manager_secret_version.tfm_confluent_api_secret.secret_data
+
   labels = {
     environment = local.env_name
     managed_by  = "terraform"
   }
 
   module "google_gke_cluster" {
-    source = "../../modules/gke_cluster"
+    source = "../../modules/confluent_config"
 
-    env_name        = local.env_name
-    prod_name       = local.prod_name
-    project_id      = local.gcp_project_id
-    region          = local.env_location
-    resource_prefix = local.resource_prefix
-    labels          = local.labels
+    env_name                    = local.env_name
+    prod_name                   = local.prod_name
+    project_id                  = local.gcp_project_id
+    region                      = local.env_location
+    resource_prefix             = local.resource_prefix
+    labels                      = local.labels
+    tfm_sa_confluent_api_key    = local.tfm_sa_confluent_api_key
+    tfm_sa_confluent_api_secret = tfm_sa_confluent_api_secret
 }

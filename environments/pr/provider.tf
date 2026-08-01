157edb587b8c57394e4897b51ef39b34dcfcb9a1
AmareshGuligoudar <amareshguligoudar@gmail.com>
2026-08-01 21:58:03 +0530

    Update provider.tf

--- environments/pr/provider.tf
+++ environments/pr/provider.tf
@@ -16,6 +16,8 @@ terraform {
 
 provider "confluent" {
   # Credentials come from CONFLUENT_CLOUD_API_KEY and CONFLUENT_CLOUD_API_SECRET.
-CONFLUENT_CLOUD_API_KEY    = local.tfm_sa_confluent_api_key
-CONFLUENT_CLOUD_API_SECRET = local.tfm_sa_confluent_api_secret
+  cloud_api_key    = var.confluent_cloud_api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
+  cloud_api_secret = var.confluent_cloud_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
 }
+  
+

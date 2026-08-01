42c4b6d5334ecc287bb62e978e9e6b60121119fb
AmareshGuligoudar <amareshguligoudar@gmail.com>
2026-08-01 22:01:28 +0530

    Fix typo in cloud_api_secret variable reference

--- environments/pr/provider.tf
+++ environments/pr/provider.tf
@@ -17,5 +17,5 @@ terraform {
 provider "confluent" {
   # Credentials come from CONFLUENT_CLOUD_API_KEY and CONFLUENT_CLOUD_API_SECRET.
   cloud_api_key    = local.tfm_sa_confluent_api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
-  cloud_api_secret = ocal.tfm_sa_confluent_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
+  cloud_api_secret = local.tfm_sa_confluent_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
 }

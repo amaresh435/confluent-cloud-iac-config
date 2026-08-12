57044cbe2e5aee322cfedb682864de7f5006349b
amaresh435 <amaresh.guligoudar.com>
2026-08-12 23:39:29 +0530

    create

--- modules/confluent_config/outputs.tf
+++ modules/confluent_config/outputs.tf
@@ -1,8 +1,3 @@
-output "environment_ids" {
-  description = "Confluent environment IDs, keyed by input environment key."
-  value       = { for key, environment in confluent_environment.gcp_dedicated : key => environment.id }
-}
-
 output "cluster_ids" {
   description = "Kafka cluster IDs, keyed by input environment key."
   value       = { for key, cluster in confluent_kafka_cluster.gcp_dedicated : key => cluster.id }
@@ -13,7 +8,12 @@ output "cluster_bootstrap_endpoints" {
   value       = { for key, cluster in confluent_kafka_cluster.gcp_dedicated : key => cluster.bootstrap_endpoint }
 }
 
+output "environment_ids" {
+  description = "Confluent environment IDs, keyed by input environment key."
+  value       = { for key, environment in confluent_environment.main : key => environment.id }
+}
+
 output "service_account_ids" {
-  description = "Created service-account IDs, keyed by input service-account key."
-  value       = { for key, account in confluent_service_account.gcp_dedicated : key => account.id }
+  description = "Confluent service account IDs, keyed by input environment key."
+  value       = { for key, account in confluent_service_account.main : key => account.id }
 }

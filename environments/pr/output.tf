544186a70f0a1743eebcd620cf034fddb6eaf8d6
amaresh435 <amaresh.guligoudar.com>
2026-08-12 22:43:29 +0530

    destory

--- environments/pr/output.tf
+++ environments/pr/output.tf
@@ -1,4 +1,5 @@
-output "confluent_config" {
-  value     = module.confluent_config
-  sensitive = true
-}
+#output "confluent_config" {
+#  value     = module.confluent_config
+#  sensitive = true
+#}
+#

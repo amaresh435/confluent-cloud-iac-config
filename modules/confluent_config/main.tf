92a2af5c1dfb613414d46c3ea5b520057e934555
amaresh435 <amaresh.guligoudar.com>
2026-08-13 00:15:34 +0530

    create

--- modules/confluent_config/main.tf
+++ modules/confluent_config/main.tf
@@ -23,7 +23,7 @@ resource "confluent_environment" "main" {
 resource "confluent_kafka_cluster" "gcp_dedicated" {
   for_each = toset(var.confluent_cltr_name)
 
-  display_name        = join("-", ["var.resource_prefix", "each.key"])
+  display_name        = join("-", [var.resource_prefix, "each.key"])
   availability        = "SINGLE_ZONE"
   cloud               = "GCP"
   region              = local.gcp_region
